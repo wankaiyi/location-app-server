6140cc3d0db205603900fe0ec01de92541c2710d
wankaiyi <wankaiyi123@163.com>
2025-11-06 16:38:48 +0800

    依赖

---
+++ src/main/java/com/wky/utils/CoordinateTransformUtils.java
@@ -0,0 +1,99 @@
+package com.wky.utils;
+
+/**
+ * @author wky
+ * @date 2025/11/06
+ */
+public class CoordinateTransformUtils {
+
+    private static final double PI = 3.1415926535897932384626;
+    private static final double A = 6378245.0;
+    private static final double EE = 0.00669342162296594323;
+
+    /**
+     * WGS-84 转 GCJ-02（火星坐标）
+     *
+     * @param wgsLat WGS-84 纬度
+     * @param wgsLng WGS-84 经度
+     * @return double[] {gcjLat, gcjLng}
+     */
+    public static double[] wgs84ToGcj02(double wgsLat, double wgsLng) {
+        if (outOfChina(wgsLat, wgsLng)) {
+            return new double[]{wgsLat, wgsLng};
+        }
+
+        double dLat = transformLat(wgsLng - 105.0, wgsLat - 35.0);
+        double dLng = transformLng(wgsLng - 105.0, wgsLat - 35.0);
+
+        double radLat = wgsLat / 180.0 * PI;
+        double magic = Math.sin(radLat);
+        magic = 1 - EE * magic * magic;
+        double sqrtMagic = Math.sqrt(magic);
+
+        dLat = (dLat * 180.0) / ((A * (1 - EE)) / (magic * sqrtMagic) * PI);
+        dLng = (dLng * 180.0) / (A / sqrtMagic * Math.cos(radLat) * PI);
+
+        double gcjLat = wgsLat + dLat;
+        double gcjLng = wgsLng + dLng;
+
+        return new double[]{gcjLat, gcjLng};
+    }
+
+    /**
+     * GCJ-02（火星坐标）转 WGS-84
+     *
+     * @param gcjLat GCJ-02 纬度
+     * @param gcjLng GCJ-02 经度
+     * @return double[] {wgsLat, wgsLng}
+     */
+    public static double[] gcj02ToWgs84(double gcjLat, double gcjLng) {
+        if (outOfChina(gcjLat, gcjLng)) {
+            return new double[]{gcjLat, gcjLng};
+        }
+
+        double dLat = transformLat(gcjLng - 105.0, gcjLat - 35.0);
+        double dLng = transformLng(gcjLng - 105.0, gcjLat - 35.0);
+
+        double radLat = gcjLat / 180.0 * PI;
+        double magic = Math.sin(radLat);
+        magic = 1 - EE * magic * magic;
+        double sqrtMagic = Math.sqrt(magic);
+
+        dLat = (dLat * 180.0) / ((A * (1 - EE)) / (magic * sqrtMagic) * PI);
+        dLng = (dLng * 180.0) / (A / sqrtMagic * Math.cos(radLat) * PI);
+
+        double wgsLat = gcjLat - dLat;
+        double wgsLng = gcjLng - dLng;
+
+        return new double[]{wgsLat, wgsLng};
+    }
+
+    private static double transformLat(double x, double y) {
+        double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * PI) + 20.0 * Math.sin(2.0 * x * PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(y * PI) + 40.0 * Math.sin(y / 3.0 * PI)) * 2.0 / 3.0;
+        ret += (160.0 * Math.sin(y / 12.0 * PI) + 320 * Math.sin(y * PI / 30.0)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    private static double transformLng(double x, double y) {
+        double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * PI) + 20.0 * Math.sin(2.0 * x * PI)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(x * PI) + 40.0 * Math.sin(x / 3.0 * PI)) * 2.0 / 3.0;
+        ret += (150.0 * Math.sin(x / 12.0 * PI) + 300.0 * Math.sin(x / 30.0 * PI)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    /**
+     * 判断坐标是否在中国以外
+     */
+    public static boolean outOfChina(double lat, double lng) {
+        if (lng < 72.004 || lng > 137.8347) {
+            return true;
+        }
+        if (lat < 0.8293 || lat > 55.8271) {
+            return true;
+        }
+        return false;
+    }
+}
